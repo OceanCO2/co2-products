73a1739db15a645a0f13bbd07e8823fc52c0dc7d
lukegre <lukegre@gmail.com>
2025-11-03 17:50:39 +0100

    added contact email.

--- src/co2_synthesis/config.py
+++ src/co2_synthesis/config.py
@@ -7,23 +7,21 @@ dotenv.load_dotenv()
 
 class Config(BaseSettings):
     LOGGER_LEVEL: str = 'DEBUG'
+    ROOT: pathlib.Path = pathlib.Path(dotenv.find_dotenv('.env')).parent.resolve()
+    
     GOOGLE_SHEET_ID: str
     GOOGLE_TAB_ID: str
 
     GITHUB_REPO: str = 'https://github.com/OceanCO2/co2-products'
-
-    ROOT: pathlib.Path = pathlib.Path(dotenv.find_dotenv('.env')).parent.resolve()
+    SUBMISSION_FORM: str = "https://docs.google.com/forms/d/e/1FAIpQLSdguEed1sRdgHjhP_KsgHeh_G7gHjz8qGmuTu9C7K_fK7hyUQ/viewform"
+    CONTACT_EMAIL: str = "noaa.ocads@noaa.gov"
 
     # website settings
     WEBSITE_TITLE: str = "Ocean CO2 Products"
     WEBSITE_DESCRIPTION: str = "Ocean carbon products from Jiang et al. (2025)"
-    WEBSITE_COPY: str = "&copy; Website by Luke Gregor (2025)"
-    WEBSITE_INFOS_HTML: str = """
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdguEed1sRdgHjhP_KsgHeh_G7gHjz8qGmuTu9C7K_fK7hyUQ/viewform" target="_blank">Submit a new dataset</a>
-        <br>
+    WEBSITE_COPY: str = "&copy; Website by <a href='https://github.com/lukegre/'>Luke Gregor</a> (2025)"
 
-    """
-
+    # Filter settings
     DATA_FILTERS: tuple[str, ...] = (
         "Data Type",
         "Spatial domains",
